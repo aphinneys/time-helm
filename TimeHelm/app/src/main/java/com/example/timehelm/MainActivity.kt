94256cef854d41134c02289dc439cbf9106a5ce5
Anita Silver <aps66@duke.edu>
2022-12-04 12:56:34 -0500

    more messing around

--- TimeHelm/app/src/main/java/com/example/timehelm/MainActivity.kt
+++ TimeHelm/app/src/main/java/com/example/timehelm/MainActivity.kt
@@ -3,14 +3,21 @@ package com.example.timehelm
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.timehelm.ui.theme.TimeHelmTheme
+
+var fucks_tally = 0
+var streak = 0
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +29,19 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    Greeting("Time Helm")
+                }
+                //a BUTTON
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .fillMaxHeight()
+                        .padding(20.dp)) {
+                    Button(onClick = {
+                    }) {
+                        Text("Give a Shit")
+                    }
                 }
             }
         }
@@ -31,13 +50,23 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Text(text = "Welcome to $name!")
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     TimeHelmTheme {
-        Greeting("Android")
+        Greeting("Time Helm")
     }
 }
+
+fun MidnightTally(){
+    if (10 < fucks_tally and fucks_tally < 17) {
+        streak += 1
+    } else{
+        streak = 0
+    }
+    Text("$streak Day Sustainable Fuckery Streak!")
+}
+
