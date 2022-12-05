9d10429417657d2584f2bf1dd2e0b18ca6000d57
Ava Silver <avapsilver@gmail.com>
2022-12-05 00:28:45 -0500

    fixed more bugs

--- app/src/main/java/com/example/timehelm/MainActivity.kt
+++ app/src/main/java/com/example/timehelm/MainActivity.kt
@@ -5,8 +5,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.*
@@ -15,12 +17,15 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.timehelm.TimeState.Tracking
 import com.example.timehelm.ui.theme.TimeHelmTheme
 import com.google.protobuf.Timestamp
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
+import java.time.LocalDate
+import java.time.ZoneId
 import kotlin.math.min
 
 
@@ -53,20 +58,6 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             TimeHelmTheme {
-                var selectedIndex by remember { mutableStateOf(0) }
-                val state: TimeState by LocalContext.current.stateDataStore.data.collectAsState(
-                    initial = TimeState.getDefaultInstance()
-                ) // how to use state
-                val context = LocalContext.current
-                val scope = rememberCoroutineScope()
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Time Helm")
-                }
-                //a BUTTON
                 Column(
                     horizontalAlignment = Alignment.CenterHorizontally,
                     modifier = Modifier
@@ -74,36 +65,36 @@ class MainActivity : ComponentActivity() {
                         .fillMaxHeight()
                         .padding(20.dp)
                 ) {
+                    Text(text = "Welcome to Time Helm!", fontSize = 30.sp, modifier = Modifier.padding(10.dp))
+                    var selectedIndex by remember { mutableStateOf(0) }
+                    val state: TimeState by LocalContext.current.stateDataStore.data.collectAsState(
+                        initial = TimeState.getDefaultInstance()
+                    ) // how to use state
+                    val context = LocalContext.current
+                    val scope = rememberCoroutineScope()
                     val now = now()
-                    val firstOpenToday = state.lastDayStreak.seconds < now.seconds
+                    val startOfDay = LocalDate.now().atStartOfDay().atZone(ZoneId.systemDefault()).toInstant().epochSecond
+                    val firstOpenToday = state.lastDayStreak.seconds < startOfDay
                     if (firstOpenToday) {
-                        if (state.fucksTally in 10.0..15.0){
-                            updateState(scope, context) {
-                                it.setStreakDays(it.streakDays + 1)
-                            }}
-                        } else {
                         updateState(scope, context) {
-                            it.setStreakDays(0) // lost the streak!
-                        }}
-                        updateState(scope, context) {
-                            it.setFucksTally(0F).setLastDayStreak(now)
+                            it.setStreakDays(if (state.fucksTally in 10.0..15.0) it.streakDays + 1 else 0)
+                                .setFucksTally(0F).setLastDayStreak(now)
                         }
-                }
-                Text("${state.streakDays} Day Sustainable Fuckery Streak!")
-                //todo : include XP in proto save file thingy, and load it here...
-                val XP =  (2..8).random()
-                var tracking = state.hasTracking()
-                // val thing = state.fucksTally
-                // Text("we have a state!!! $thing")
-
-                if (tracking) {
-                    StopGivingAFuckButton(state = state) //stopTracking = { tracking = false })
-                    CheckInButton(state = state)
-                } else {
-                    GiveAFuckButton(state = state, setidx = selectedIndex)
-                    Text("select an activity lol")
-                    DropdownDemo(selectedIndex, setIndex = { selectedIndex = it })
-
+                    }
+                    Text("${state.streakDays} Day Sustainable Fuckery Streak!")
+                    //todo : include XP in proto save file thingy, and load it here...
+                    val XP =  (2..8).random()
+                    val tracking = state.hasTracking()
+                    var time by remember { mutableStateOf(now()) }
+                    ProgressIndicator(state = state, time = time)
+                    if (tracking) {
+                        StopGivingAFuckButton() //stopTracking = { tracking = false })
+                        CheckInButton(update = { time = now() })
+                    } else {
+                        GiveAFuckButton(setIdx = selectedIndex)
+                        Text("select an activity lol")
+                        DropdownDemo(selectedIndex, setIndex = { selectedIndex = it })
+                    }
                 }
             }
         }
@@ -117,56 +108,67 @@ fun updateState(scope: CoroutineScope, context: Context, update: (TimeState.Buil
     }
 }
 
-fun fucksToday(state: TimeState): Float {
-    val elapsed = now().seconds - state.tracking.startTime.seconds
-    return state.fucksTally + elapsed / 3600f * rates[Activities.values()[state.tracking.activityValue]]!!
+fun fucksToday(tracking: Tracking, fucksTally: Float, time: Timestamp? = null): Float {
+    val since = time ?: now()
+    val elapsed = since.seconds - tracking.startTime.seconds
+    return fucksTally + elapsed / 3600f * rates[Activities.values()[tracking.activityValue]]!!
 }
 
 
 @Composable
-fun CheckInButton(state: TimeState) {
-    var expanded by remember { mutableStateOf(false) }
-    Button(onClick = { expanded = !expanded}, shape = RoundedCornerShape(50)) {
+fun CheckInButton(update: () -> Unit) {
+    val scope = rememberCoroutineScope()
+    val context = LocalContext.current
+    Button(onClick = update, shape = RoundedCornerShape(50)) {
         Text("check progress")
     }
-    val fucksToday = fucksToday(state)
+}
+
+@Composable
+fun ProgressIndicator(state: TimeState, time: Timestamp) {
+    val fucksToday = fucksToday(state.tracking, state.fucksTally, time = time)
     // this is the same function as the stop button, returns same info, just doesn't "stop" , maybe better to combine somehow?
-    if (expanded) {
-        CircularProgressIndicator(min(1f, fucksToday / 10f))
-        if (fucksToday > 15) {
-            Text("Please stop working, it's been a long day.")
-        }
+
+    CircularProgressIndicator(min(1f, fucksToday * 5),
+        modifier = Modifier.border(
+            width = 1.dp,
+            color = Color.Black,
+            shape = CircleShape)
+            .size(120.dp),
+        strokeWidth = 20.dp)
+    if (fucksToday > 15) {
+        Text("Please stop working, it's been a long day.")
     }
 }
 
 //https://www.baeldung.com/kotlin/builder-pattern#implementation
 
 @Composable
-fun GiveAFuckButton(state: TimeState, setidx: Int) {
+fun GiveAFuckButton(setIdx: Int) {
     val context = LocalContext.current
     val scope = rememberCoroutineScope()
     Button(onClick = {
-        val ts = com.google.protobuf.Timestamp.newBuilder().build()
         updateState(scope, context) {
-            it.setTracking(TimeState.Tracking.newBuilder().setStartTime(ts).setActivityValue(setidx).build())
+            it.setTracking(TimeState.Tracking.newBuilder().setStartTime(now())
+                .setActivityValue(setIdx).build())
                 //add a set XP: decrement by 1 IF current time is before 10am
             // decrement XP if this is the second time today...
-        }}
-        , shape = RoundedCornerShape(50)) {
+        }},
+        shape = RoundedCornerShape(50)) {
         Text("give a fuck :)")
     }
 }
 
 @Composable
-fun StopGivingAFuckButton(state: TimeState) { //stopTracking: () -> Unit) {
+fun StopGivingAFuckButton() {
     val context = LocalContext.current
     val scope = rememberCoroutineScope()
     Button(
         onClick = {
-            val ts = com.google.protobuf.Timestamp.newBuilder().build()
-            val elapsed = ts.seconds - state.tracking.startTime.seconds
-            val fuckstoday = state.fucksTally + elapsed * state.tracking.activityValue
-            updateState(scope, context) { it.clearTracking().setFucksTally(fuckstoday) }
+            updateState(scope, context) {
+                it.setFucksTally(fucksToday(it.tracking, it.fucksTally))
+                .clearTracking()
+            }
         },
         //add a set XP: decrement by 1 IF fuckstoday > 3 and time is before noon
         // if 5 fucks before 5pm
@@ -181,11 +183,6 @@ fun StopGivingAFuckButton(state: TimeState) { //stopTracking: () -> Unit) {
         shape = RoundedCornerShape(50),
     ) {
         Text("Ok we done giving a fuck")
-    }
-    val fucksToday = fucksToday(state)
-    CircularProgressIndicator(progress = min(1f, fucksToday / 10f))
-    if (fucksToday > 15) {
-        Text("Please stop working, it's been a long day.")
     }
 }
 
@@ -224,31 +221,3 @@ fun DropdownDemo(selectedIndex: Int, setIndex: (Int) -> Unit) {
         }
     }
 }
-
-
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Welcome to $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    TimeHelmTheme {
-        Greeting("Time Helm")
-    }
-}
-
-
-//
-//@Composable
-//fun MidnightTally(){
-//    if (fucks_tally in 10..16) {
-//        streak += 1
-//    } else{
-//        streak = 0
-//    }
-//    Text("$streak Day Sustainable Fuckery Streak!")
-//}
-
