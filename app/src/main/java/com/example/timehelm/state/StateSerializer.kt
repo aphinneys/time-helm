3af00a61ca59cf9c6e1f153eff25b03a370fa21a
Ava Silver <avapsilver@gmail.com>
2022-12-30 21:35:21 -0700

    refactored state logic

--- app/src/main/java/com/example/timehelm/state/StateSerializer.kt
+++ app/src/main/java/com/example/timehelm/state/StateSerializer.kt
@@ -6,88 +6,60 @@ import androidx.datastore.core.DataStore
 import androidx.datastore.core.Serializer
 import androidx.datastore.dataStore
 import com.google.protobuf.InvalidProtocolBufferException
+import com.google.protobuf.MessageLite
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 import java.io.InputStream
 import java.io.OutputStream
 
-// TIME STATE
 
-object StateSerializer : Serializer<State> {
-  override val defaultValue: State = State.getDefaultInstance()
+@Suppress("UNCHECKED_CAST")
+fun <T : MessageLite, B : MessageLite.Builder> updateHook(
+  scope: CoroutineScope,
+  context: Context,
+  accessState: (Context) -> DataStore<T>,
+): (((B) -> B) -> Unit) {
+  return { update ->
+    scope.launch {
+      accessState(context).updateData { currentState ->
+        currentState.toBuilder().also { update(it as B) }.build() as T
+      }
+    }
+  }
+}
 
-  override suspend fun readFrom(input: InputStream): State {
+open class Serialize<T : MessageLite>(default: () -> T, val parseFrom: (InputStream) -> T) :
+  Serializer<T> {
+  override val defaultValue: T = default()
+  override suspend fun readFrom(input: InputStream): T {
     try {
-      return State.parseFrom(input)
+      return parseFrom(input)
     } catch (exception: InvalidProtocolBufferException) {
       throw CorruptionException("Cannot read proto.", exception)
     }
   }
 
   override suspend fun writeTo(
-    t: State,
+    t: T,
     output: OutputStream
   ) = t.writeTo(output)
 }
 
-val Context.stateDataStore: DataStore<State> by dataStore(
-  fileName = "state.pb",
-  serializer = StateSerializer
-)
+// TIME STATE
 
+object StateSerializer : Serialize<State>({ State.getDefaultInstance() }, { State.parseFrom(it) })
+val Context.stateDataStore: DataStore<State> by dataStore("state.pb", StateSerializer)
 typealias StateUpdate = ((State.Builder) -> State.Builder) -> Unit
-
-fun useUpdateState(
-  scope: CoroutineScope,
-  context: Context
-): StateUpdate {
-  return { update ->
-    scope.launch {
-      context.stateDataStore.updateData { currentState ->
-        currentState.toBuilder().also { update(it) }.build()
-      }
-    }
-  }
-}
+fun useUpdateState(scope: CoroutineScope, context: Context): StateUpdate =
+  updateHook(scope, context) { it.stateDataStore }
 
 // SETTINGS
 
-object SettingsSerializer : Serializer<Settings> {
-  override val defaultValue: Settings = Settings.getDefaultInstance()
-
-  override suspend fun readFrom(input: InputStream): Settings {
-    try {
-      return Settings.parseFrom(input)
-    } catch (exception: InvalidProtocolBufferException) {
-      throw CorruptionException("Cannot read proto.", exception)
-    }
-  }
-
-  override suspend fun writeTo(
-    t: Settings,
-    output: OutputStream
-  ) = t.writeTo(output)
-}
-
-val Context.settingsDataStore: DataStore<Settings> by dataStore(
-  fileName = "settings.pb",
-  serializer = SettingsSerializer
-)
-
+object SettingsSerializer : Serialize<Settings>({Settings.getDefaultInstance()}, {Settings.parseFrom(it)})
+val Context.settingsDataStore: DataStore<Settings> by dataStore("settings.pb", SettingsSerializer)
 typealias SettingsUpdate = ((Settings.Builder) -> Settings.Builder) -> Unit
-
-fun useUpdateSettings(
-  scope: CoroutineScope,
-  context: Context
-): SettingsUpdate {
-  return { update ->
-    scope.launch {
-      context.settingsDataStore.updateData { currentSettings ->
-        currentSettings.toBuilder().also { update(it) }.build()
-      }
-    }
-  }
-}
+fun useUpdateSettings(scope: CoroutineScope, context: Context): SettingsUpdate =
+  updateHook(scope, context) { it.settingsDataStore }
 
 fun Settings.isFullyInitialized(): Boolean {
   return isInitialized && dailyHoursMax >= 0 && dailyHoursMin >= 0 && dailyHoursMax >= 0 && difficultyAvg >= 0 && difficultyVariance >= 0
@@ -95,39 +67,8 @@ fun Settings.isFullyInitialized(): Boolean {
 
 // POKEMON STATE
 
-object PokemonSerializer : Serializer<PokemonState> {
-  override val defaultValue: PokemonState = PokemonState.getDefaultInstance()
-
-  override suspend fun readFrom(input: InputStream): PokemonState {
-    try {
-      return PokemonState.parseFrom(input)
-    } catch (exception: InvalidProtocolBufferException) {
-      throw CorruptionException("Cannot read proto.", exception)
-    }
-  }
-
-  override suspend fun writeTo(
-    t: PokemonState,
-    output: OutputStream
-  ) = t.writeTo(output)
-}
-
-val Context.pokemonDataStore: DataStore<PokemonState> by dataStore(
-  fileName = "pokemon.pb",
-  serializer = PokemonSerializer
-)
-
+object PokemonSerializer : Serialize<PokemonState>( {PokemonState.getDefaultInstance()}, {PokemonState.parseFrom(it)})
+val Context.pokemonDataStore: DataStore<PokemonState> by dataStore("pokemon.pb", PokemonSerializer)
 typealias PokemonUpdate = ((PokemonState.Builder) -> PokemonState.Builder) -> Unit
-
-fun useUpdatePokemon(
-  scope: CoroutineScope,
-  context: Context
-): PokemonUpdate {
-  return { update ->
-    scope.launch {
-      context.pokemonDataStore.updateData { currentPokemon ->
-        currentPokemon.toBuilder().also { update(it) }.build()
-      }
-    }
-  }
-}
+fun useUpdatePokemon(scope: CoroutineScope, context: Context): PokemonUpdate =
+  updateHook(scope, context) {it.pokemonDataStore}
