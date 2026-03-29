e53eaf82e72ce6751209631cec504e0d37d0c411
Ava Silver <avapsilver@gmail.com>
2026-03-29 13:35:36 -0400

    update to version 6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.timehelm.timehelm"
         minSdk = 29
         targetSdk = 36
-        versionCode = 5
-        versionName = "1.1"
+        versionCode = 6
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
